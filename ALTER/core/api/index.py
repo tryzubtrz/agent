be5cc8b2c3fe60ec29c82f5c43a9550daf0f247d
tryzubtrz <tryzub.trz@gmail.com>
2026-08-26 03:36:30 -0400

    feat: register memory admin APIs

--- ALTER/core/api/index.py
+++ ALTER/core/api/index.py
@@ -3,6 +3,7 @@ from alter_core.approval_api import router as approval_router
 from alter_core.connector_gateway_api import router as connector_gateway_router
 from alter_core.conversation_api import router as conversation_router
 from alter_core.document_api import router as document_router
+from alter_core.memory_admin_api import router as memory_admin_router
 from alter_core.model_api import router as model_router
 from alter_core.policy_admin_api import router as policy_admin_router
 from alter_core.productivity_api import router as productivity_router
@@ -15,6 +16,7 @@ app.include_router(approval_router)
 app.include_router(connector_gateway_router)
 app.include_router(conversation_router)
 app.include_router(document_router)
+app.include_router(memory_admin_router)
 app.include_router(model_router)
 app.include_router(policy_admin_router)
 app.include_router(productivity_router)
