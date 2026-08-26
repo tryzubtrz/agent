7e33455b9f879c0ae1a64f4bc9867c632e119838
tryzubtrz <tryzub.trz@gmail.com>
2026-08-26 03:33:05 -0400

    feat: register document APIs

--- ALTER/core/api/index.py
+++ ALTER/core/api/index.py
@@ -2,6 +2,7 @@ from alter_core.agent_api import router as agent_router
 from alter_core.approval_api import router as approval_router
 from alter_core.connector_gateway_api import router as connector_gateway_router
 from alter_core.conversation_api import router as conversation_router
+from alter_core.document_api import router as document_router
 from alter_core.model_api import router as model_router
 from alter_core.productivity_api import router as productivity_router
 from alter_core.system_api import router as system_router
@@ -12,6 +13,7 @@ app.include_router(agent_router)
 app.include_router(approval_router)
 app.include_router(connector_gateway_router)
 app.include_router(conversation_router)
+app.include_router(document_router)
 app.include_router(model_router)
 app.include_router(productivity_router)
 app.include_router(system_router)
