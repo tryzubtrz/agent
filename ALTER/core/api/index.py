cd2ff732c451eabad43f5dd4ee632fc41e00c348
tryzubtrz <tryzub.trz@gmail.com>
2026-08-26 02:44:56 -0400

    feat: mount connector gateway API

--- ALTER/core/api/index.py
+++ ALTER/core/api/index.py
@@ -1,5 +1,6 @@
 from alter_core.agent_api import router as agent_router
 from alter_core.approval_api import router as approval_router
+from alter_core.connector_gateway_api import router as connector_gateway_router
 from alter_core.conversation_api import router as conversation_router
 from alter_core.model_api import router as model_router
 from alter_core.system_api import router as system_router
@@ -8,6 +9,7 @@ from alter_core.api import app
 
 app.include_router(agent_router)
 app.include_router(approval_router)
+app.include_router(connector_gateway_router)
 app.include_router(conversation_router)
 app.include_router(model_router)
 app.include_router(system_router)
