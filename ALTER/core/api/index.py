680072ea91b3ff41e0ce96fc2a4648d9d2a96a04
tryzubtrz <tryzub.trz@gmail.com>
2026-08-26 02:30:09 -0400

    feat: mount vault and model router APIs

--- ALTER/core/api/index.py
+++ ALTER/core/api/index.py
@@ -1,6 +1,10 @@
 from alter_core.agent_api import router as agent_router
 from alter_core.approval_api import router as approval_router
+from alter_core.model_api import router as model_router
+from alter_core.vault_api import router as vault_router
 from alter_core.api import app
 
 app.include_router(agent_router)
 app.include_router(approval_router)
+app.include_router(model_router)
+app.include_router(vault_router)
