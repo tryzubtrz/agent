403131d9088d79d62f38c7862cd36d61819cfbfb
tryzubtrz <tryzub.trz@gmail.com>
2026-08-26 02:26:33 -0400

    feat: mount approval workflow routes

--- ALTER/core/api/index.py
+++ ALTER/core/api/index.py
@@ -1,4 +1,6 @@
 from alter_core.agent_api import router as agent_router
+from alter_core.approval_api import router as approval_router
 from alter_core.api import app
 
 app.include_router(agent_router)
+app.include_router(approval_router)
