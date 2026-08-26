950e46d8338084afa316ce1f7e99861ae6fa48d8
tryzubtrz <tryzub.trz@gmail.com>
2026-08-26 02:39:26 -0400

    feat: mount conversation and system status APIs

--- ALTER/core/api/index.py
+++ ALTER/core/api/index.py
@@ -1,10 +1,14 @@
 from alter_core.agent_api import router as agent_router
 from alter_core.approval_api import router as approval_router
+from alter_core.conversation_api import router as conversation_router
 from alter_core.model_api import router as model_router
+from alter_core.system_api import router as system_router
 from alter_core.vault_api import router as vault_router
 from alter_core.api import app
 
 app.include_router(agent_router)
 app.include_router(approval_router)
+app.include_router(conversation_router)
 app.include_router(model_router)
+app.include_router(system_router)
 app.include_router(vault_router)
